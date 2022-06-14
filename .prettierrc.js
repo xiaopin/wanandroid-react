bb94e0622d9cef7738df5531f90a4ed9dbfab86e
Pincheng Wu <pincheng23@hotmail.com>
2022-06-14 14:54:08 +0800

    Init

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // 是否添加分号
+    semi: false,
+    // 是否使用单引号
+    singleQuote: true,
+    // 单行代码的最大长度
+    printWidth: 150,
+    // 是否缩进<script>和<style>内的代码(v1.19.0+)
+    vueIndentScriptAndStyle: false,
+    // 是否换行(always:当超出printWidth时就换行 never:不换行 perserve:按文件原样换行)(v1.8.2+)
+    proseWrap: 'never',
+    // 对象的 key 仅在必要时用引号
+    quoteProps: 'as-needed',
+    // 末尾不需要逗号
+    trailingComma: 'none',
+    // 大括号内的首尾需要空格
+    bracketSpacing: true,
+    // 箭头函数只有一个参数时, 不添加括号
+    arrowParens: 'avoid',
+    // 缩进
+    tabWidth: 4
+}
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@/': `${resolve(__dirname, 'src')}/`
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // 全局引入 scss 文件
+                additionalData: '@use "./src/style/variable.scss" as *;'
+            }
+        }
+    }
+})
